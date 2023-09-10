545ea4c5644c8305713d71fec3f0ee4019e028a2
Laddu12321 <144617387+Laddu12321@users.noreply.github.com>
2023-09-10 22:40:05 +0530

    Add files via upload

---
+++ asc.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,a[10],n;
+cout<<"n value";
+cin>>n;
+cout<<"enter array"<<endl;
+for(i=0;i<n;i++)
+cin>>a[i];
+for(i=0;i<n;i++)
+{
+for(j=0;j<n-1;j++)
+{
+if(a[j]>a[j+1])
+{
+int temp=a[j+1];
+a[j+1]=a[j];
+a[j]=temp;
+}
+}
+}
+cout<<"acending\n";
+for(i=0;i<n;i++)
+cout<<a[i];
+return 0;
+}
